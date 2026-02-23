1aa7bd2afed1c6355aa07acd5b3df22fddc5a00a
Profcode <imwangi31@gmail.com>
2026-02-23 17:12:12 +0300

    Foundations 1-5

--- foundations/04_mathEquations/mathEquations.js
+++ foundations/04_mathEquations/mathEquations.js
@@ -1,9 +1,9 @@
 const a = 1 - 1 // Freebie!!! This is the answer to "one minus one"
-const b = "one plus eight"
-const c = "22 times three"
-const d = "the *remainder* of 5/4"
-const e = "the variable 'b' minus 17"
-const f = "the sum of the previous five variables"
+const b = 1 + 8 //"one plus eight"
+const c = 22 * 3  //"22 times three"
+const d = 1  //"the *remainder* of 5/4"
+const e = 9 - 17  //"the variable 'b' minus 17"
+const f = 0 + 9 + 66 + 1 - 8 //"the sum of the previous five variables"
 
 // Do not edit below this line
 module.exports = {a, b, c, d, e, f}
